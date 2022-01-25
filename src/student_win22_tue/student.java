61c77e85ed5787a0dcfa3e589693e4e24944b51c
atouts <atouts@sheridancollege.ca>
2022-01-25 12:57:35 -0500

    added address label in student.java

--- src/student_win22_tue/student.java
+++ src/student_win22_tue/student.java
@@ -5,11 +5,20 @@ package student_win22_tue;
 public class student {
   private String studentId;
   private String studentName;
+  private String address;
   
   public student(String studentId,String studentName){
       this.studentId=studentId;
       this.studentName=studentName;
   }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getStudentId() {
         return studentId;
